9af602344d74771e6a8908f9a2d8e2ddb5e3e6ca
Matt Sam <MSTTmm1@gmail.com>
2021-09-18 21:13:25 -0400

    。。。

--- aimbot-exec/now/AI_M_BOT_N.py
+++ aimbot-exec/now/AI_M_BOT_N.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     freeze_support()
 
     Copyright_Information = '\n\n\n=========================\n| FREE to use 本软件免费使用\n| Author/作者: jiapai12138(killmatt01)\n| 学习交流欢迎加群: 212068326\n========================='
-    MsgBox('v2.6.4 版本更新', 'bug生成/修复\n更换模型,对cf/cs1.6/逆战支持更好\n暂时不用yolox模型(等待训练)'+Copyright_Information)
+    MsgBox('v2.6.4 版本更新', 'bug生成/修复\n更换权重,对cf/cs1.6/逆战支持更好\n暂时不用yolox模型(等待训练)'+Copyright_Information)
 
     MsgBox('键鼠检测', f'罗技驱动加载状态: {gmok} (函数已稳定)\n飞易来/文盒驱动准备状态: {msdkok} (函数测试中)')
 
